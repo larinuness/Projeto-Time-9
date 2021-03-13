a6618edf74d3751cd7b9d8c6f6e1f261dbfb199f
Dornelas <julianadornelas71@gmail.com>
2021-03-13 14:14:40 -0300

    formulario colaborador

---
+++ coleta/src/views/FormColab.vue
@@ -0,0 +1,150 @@
+<template>
+    <v-container>
+        <h3>Cadastre-se e vire torne um colaborador</h3>
+<form @submit.prevent="submit">
+     
+        <v-text-field
+          v-model="nome"
+          :error-messages="errors"
+          label="Nome"
+          required
+        ></v-text-field>
+    
+      
+        <v-text-field
+          v-model="telefone"
+          :error-messages="errors"
+          label="Telefone"
+          required
+        ></v-text-field>
+      
+      
+        <v-text-field
+          v-model="endereco"
+          :error-messages="errors"
+          label="Endereço"
+          required
+        ></v-text-field>
+     
+     
+     <h5 class="selecione-cat">Selecione as categorias desejadas:</h5> 
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Aço"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+     <v-checkbox
+          v-model="checkbox"
+          value="2"
+          label="Óleo de cozinha"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+         <v-checkbox
+          v-model="checkbox"
+          value="3"
+          label="Borracha"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+         <v-checkbox
+          v-model="checkbox"
+          value="4"
+          label="Pilhas"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+         <v-checkbox
+          v-model="checkbox"
+          value="5"
+          label="Capsulas de café"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+         <v-checkbox
+          v-model="checkbox "
+          value="6"
+          label="Isopor"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+         <v-checkbox
+          v-model="checkbox"
+          value="7"
+          label="Remédios"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+         <v-checkbox
+          v-model="checkbox"
+          value="8"
+          label="Entulhos"
+          type="checkbox"
+          required
+        ></v-checkbox>
+
+      <v-btn
+        class="mr-4"
+        type="submit"
+        :disabled="invalid"
+      >
+        Enviar
+      </v-btn>
+      <v-btn @click="clear">
+        Limpar campos
+      </v-btn>
+    </form>
+    </v-container>
+</template>
+
+<script>
+  
+
+  export default {
+
+    data: () => ({
+      nome: '',
+      telefone: '',
+      endereco: '',
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: [ ]
+    }),
+
+    methods: {
+      submit () {
+        this.$refs.observer.validate()
+      },
+      clear () {
+        this.name = ''
+        this.phoneNumber = ''
+        this.email = ''
+        this.select = null
+        this.checkbox = null
+        this.$refs.observer.reset()
+      },
+    },
+  }
+</script>
+
+<style scoped>
+.selecione-cat {
+    text-align: left;
+    padding-top: 2vh;
+}
+</style>
